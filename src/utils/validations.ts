c5c1ae03a60a689269d16db1d3ad67c1702565d2
Eric H <ericolebusiness@gmail.com>
2024-06-28 20:01:48 -0400

    changed firstName and lastName input validations

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,6 +1,6 @@
 import { allCities } from './all-cities';
 
-const StartWithTwoCharactersNoNumbers = /^[A-Za-z]{2}[A-Za-z\s.]*$/;
+const lettersOnly = /^[a-zA-Z]+$/;
 
 export function isEmailValid(emailAddress: string) {
   // eslint-disable-next-line no-useless-escape
@@ -9,13 +9,13 @@ export function isEmailValid(emailAddress: string) {
 }
 
 export function isFirstNameInputValid(firstName: string) {
-  if (firstName.length > 1 && StartWithTwoCharactersNoNumbers.test(firstName)) {
+  if (firstName.length > 1 && lettersOnly.test(firstName)) {
     return true;
   }
 }
 
 export function isLastNameInputValid(lastName: string) {
-  if (lastName.length > 1 && StartWithTwoCharactersNoNumbers.test(lastName)) {
+  if (lastName.length > 1 && lettersOnly.test(lastName)) {
     return true;
   }
 }
